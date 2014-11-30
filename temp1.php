6dcc9fc4aa66766743b9f5edbd22ea21ecfe3b0f
Nerguth <Nerguth@192.168.2.100>
2014-11-30 21:08:36 +0200

    Версия v.1.02

--- temp1.php
+++ temp1.php
@@ -12,8 +12,34 @@
 		$db_object = new PDO('sqlite:'.$db_name);
 		echo 'Файл БД открыт: '.$db_name;
 		
+		$query = '';
+		$query .= 'CREATE TABLE table1 (';
+		$query .= 'id INTEGER PRIMARY KEY,';
+		$query .= 'data TEXT';
+		$query .= ')';
+		$db_object->exec($query);
 		
+		$query1 = '';
+		$query1 .= "INSERT INTO table1 (data) VALUES ('Привет 1, SQLite!')";
+		$db_object->exec($query1);
 		
+		$query2 = '';
+		$query2 .= "INSERT INTO table1 (data) VALUES ('Привет 2, SQLite!')";
+		$db_object->exec($query2);
+		
+		$query3 = '';
+		$query3 .= "INSERT INTO table1 (data) VALUES ('Привет 3, SQLite!')";
+		$db_object->exec($query3);
+		
+		$query4 = '';
+		$query4 .= 'SELECT * FROM table1';
+		$result = $db_object->query($query4);
+		foreach ($result as $row)
+		{			
+			echo '<br />'.$row['id'].'. '.$row['data'];
+		}
+		
+		echo '<br />';
 		$db_object = null;
 		echo 'Файл БД закрыт.';
 	}
